653419c50d68cea11638f29a069fdd9604eb4020
caohongming <caohongming@jd.com>
2016-09-21 15:01:14 +0800

    commit

--- third/bootstrap-datepicker/js/bootstrap-datepicker.js
+++ third/bootstrap-datepicker/js/bootstrap-datepicker.js
@@ -1666,10 +1666,10 @@
 		datesDisabled: [],
 		endDate: Infinity,
 		forceParse: true,
-		format: 'mm/dd/yyyy',
+		format: 'yyyy-mm-dd',
 		keepEmptyValues: false,
 		keyboardNavigation: true,
-		language: 'en',
+		language: 'cn',
 		minViewMode: 0,
 		maxViewMode: 4,
 		multidate: false,
@@ -1710,6 +1710,15 @@
 			today: "Today",
 			clear: "Clear",
 			titleFormat: "MM yyyy"
+		},
+		cn: {
+			days: ["周日", "周一", "周二", "周三", "周四", "周五", "周六", "周日"],
+			daysShort: ["日", "一", "二", "三", "四", "五", "六", "七"],
+			daysMin: ["日", "一", "二", "三", "四", "五", "六", "七"],
+			months: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
+			monthsShort: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
+			today: "今天",
+			clear: "清除"
 		}
 	};
 
